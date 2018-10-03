71e03ebed3b09f1e4ced807cb94e0c1da7d3513f
wuchen <2567970322@qq.com>
2018-10-03 15:50:51 +0800

    NEXT

---
+++ src/main/java/com/wc/newjava/NewjavaApplication.java
@@ -0,0 +1,12 @@
+package com.wc.newjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewjavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewjavaApplication.class, args);
+    }
+}
